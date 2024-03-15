2cef2808637964662d5b40ae22f53525ec3ecd6d
Felix Icaza <38303370+Felix-Icaza@users.noreply.github.com>
2024-03-15 09:11:23 -0600

    add astro-critters integration

--- astro.config.mjs
+++ astro.config.mjs
@@ -2,8 +2,9 @@ import { defineConfig } from 'astro/config'
 
 import tailwind from '@astrojs/tailwind'
 import { astroImageTools } from 'astro-imagetools'
+import critters from 'astro-critters'
 
 // https://astro.build/config
 export default defineConfig({
-  integrations: [tailwind(), astroImageTools]
+  integrations: [tailwind(), astroImageTools, critters()]
 })
