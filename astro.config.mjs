ab1db983950c77ab82176a9b9a5e4b96c5b72fac
Felix Icaza <38303370+Felix-Icaza@users.noreply.github.com>
2024-03-15 09:21:37 -0600

    add astro-sitemap integration

--- astro.config.mjs
+++ astro.config.mjs
@@ -3,8 +3,35 @@ import { defineConfig } from 'astro/config'
 import tailwind from '@astrojs/tailwind'
 import { astroImageTools } from 'astro-imagetools'
 import critters from 'astro-critters'
+import sitemap from 'astro-sitemap'
+
+const website = 'https://example.com'
 
 // https://astro.build/config
 export default defineConfig({
-  integrations: [tailwind(), astroImageTools, critters()]
+  integrations: [
+    tailwind(),
+    astroImageTools,
+    critters(),
+    sitemap({
+      canonicalURL: website,
+      lastmod: new Date(),
+      createLinkInHead: false,
+      xmlns: {
+        xhtml: true
+      },
+      i18n: {
+        defaultLocale: 'es',
+        locales: {
+          es: 'es'
+        }
+      },
+      // Remove trailing slash
+      serialize(item) {
+        /* eslint-disable-next-line no-param-reassign */
+        item.url = item.url.replace(/\/$/g, '')
+        return item
+      }
+    })
+  ]
 })
