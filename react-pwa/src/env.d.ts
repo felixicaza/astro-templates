cc2697b07af813326556d1fb300368d33516b8ca
Felix Icaza <38303370+Felix-Icaza@users.noreply.github.com>
2024-03-18 08:25:05 -0600

    create react-pwa template

---
+++ react-pwa/src/env.d.ts
@@ -0,0 +1,4 @@
+/* eslint-disable @typescript-eslint/triple-slash-reference */
+/// <reference types="astro/client" />
+/// <reference types="vite-plugin-pwa/info" />
+/// <reference types="vite-plugin-pwa/vanillajs" />
